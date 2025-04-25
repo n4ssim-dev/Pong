da0038558302ed216266b80080f6e566c98535e5
Nassim <achnassim34@gmail.com>
2025-04-26 00:57:10 +0200

    Ball speed & color

--- ball.py
+++ ball.py
@@ -1,14 +1,40 @@
 import turtle
+import random
+
+LIGHT_COLORS = [
+    "lightblue",
+    "lightcyan",
+    "lightgreen",
+    "lightgrey",
+    "lightpink",
+    "lightyellow",
+    "lightcoral",
+    "lightgoldenrodyellow",
+    "lightseagreen",
+    "lightskyblue",
+    "lightslategray",
+    "lightsteelblue",
+    "lavender",
+    "mintcream",
+    "mistyrose",
+    "moccasin",
+    "navajowhite",
+    "oldlace",
+    "palegoldenrod",
+    "papayawhip"
+]
+
 
 class Ball:
     def __init__(self):
         self.ball_size = (1.5,1.5,1)
         self.ball_list = []
+        self.ball_speed = 2
         self.create_ball()
 
     def create_ball(self):
         ball = turtle.Turtle("circle")
-        ball.color("orange")
+        ball.color(random.choice(LIGHT_COLORS))
         ball.penup()
         ball.goto(0,0)
         ball.setheading(-210)
@@ -17,21 +43,26 @@ class Ball:
 
     def move_ball(self):
         ball = self.ball_list[0]
-        ball.forward(2)
+        ball.forward(self.ball_speed)
 
     def change_direction_y(self):
         ball = self.ball_list[0]
         current_heading = ball.heading()
         new_heading = 360 - current_heading
         ball.setheading(new_heading)
+        ball.color(random.choice(LIGHT_COLORS))
+        self.ball_speed += 0.5
 
     def change_direction_x(self):
         ball = self.ball_list[0]
         current_heading = ball.heading()
         new_heading = 180 - current_heading
         ball.setheading(new_heading)
+        ball.color(random.choice(LIGHT_COLORS))
+        self.ball_speed += 0.5
 
     def reset_position(self):
         ball = self.ball_list[0]
         ball.goto(0, 0)
         ball.setheading(-210)
+        self.ball_speed = 2
