f8a5dee81716cb59697827e1b7548ba7103170e0
Nassim <achnassim34@gmail.com>
2025-04-26 23:17:47 +0200

    Racket modif

--- racket.py
+++ racket.py
@@ -5,6 +5,7 @@ class Racket:
         self.racket_color = "white"
         self.player_init_position = (-350,0)
         self.bot_init_position = (350,0)
+        self.bot_speed = 7
         self.rackets = []
         self.create_player_racket()
         self.create_bot_racket()
@@ -19,18 +20,18 @@ class Racket:
 
     def bot_move_up(self):
         self.rackets[1].setheading(90)
-        self.rackets[1].shapesize(stretch_wid=1, stretch_len=5)
-        self.rackets[1].forward(5)
+        self.rackets[1].shapesize(stretch_wid=0.5, stretch_len=5)
+        self.rackets[1].forward(self.bot_speed)
 
     def bot_move_down(self):
         self.rackets[1].setheading(270)
-        self.rackets[1].shapesize(stretch_wid=1, stretch_len=5)
-        self.rackets[1].forward(5)
+        self.rackets[1].shapesize(stretch_wid=0.5, stretch_len=5)
+        self.rackets[1].forward(self.bot_speed)
 
     def create_player_racket(self):
         racket = Turtle("square")
         racket.color(self.racket_color)
-        racket.shapesize(stretch_wid=5, stretch_len=1)
+        racket.shapesize(stretch_wid=5, stretch_len=0.5)
         racket.penup()
         racket.goto(self.player_init_position)
         self.rackets.append(racket)
@@ -38,11 +39,11 @@ class Racket:
     def move_up(self):
         if self.rackets[0].ycor() < 200:
             self.rackets[0].setheading(90)
-            self.rackets[0].shapesize(stretch_wid=1, stretch_len=5)
+            self.rackets[0].shapesize(stretch_wid=0.5, stretch_len=5)
             self.rackets[0].forward(10)
 
     def move_down(self):
         if self.rackets[0].ycor() > -200:
             self.rackets[0].setheading(270)
-            self.rackets[0].shapesize(stretch_wid=1, stretch_len=5)
+            self.rackets[0].shapesize(stretch_wid=0.5, stretch_len=5)
             self.rackets[0].forward(10)
